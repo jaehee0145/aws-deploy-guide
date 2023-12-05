652585c003eb8b4092a2d83091d5c8018b8cfe1f
jaehee <jaeheepark0145@gmail.com>
2023-12-06 00:02:46 +0900

    first commit

--- app/index.ts
+++ app/index.ts
@@ -1,8 +1,8 @@
 import express from "express"
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (request, response) => {
     response.status(200).send("hello from express")
