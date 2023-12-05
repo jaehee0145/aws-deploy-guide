d3c404adcb97b5dac2262ec5edd459387a5ec395
summer <summer@summerui-MacBookPro.local>
2023-12-05 23:59:59 +0900

    first commit

---
+++ app/index.ts
@@ -0,0 +1,15 @@
+import express from "express"
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (request, response) => {
+    response.status(200).send("hello from express")
+});
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log(`App listening at port ${PORT}`);
+});
